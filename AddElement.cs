e85bc13bbefe0c60534ff22086b7383960e5a8b5
Safi <mdsafi729@gmail.com>
2023-05-06 12:58:19 +0530

    [Shamsuddin]Fix: Pushing add element file

---
+++ AddElement.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+
+namespace HashTableExample
+{
+    class AddElement
+    {
+        static void Main(string[] args)
+        {
+            Hashtable hashtable = new Hashtable();
+
+            hashtable.Add("key1", "value1");
+            hashtable.Add("key2", "value2");
+            hashtable.Add("key3", "value3");
+
+            foreach (DictionaryEntry entry in hashtable)
+            {
+                Console.WriteLine($"{entry.Key}: {entry.Value}");
+            }
+        }
+    }
+}
